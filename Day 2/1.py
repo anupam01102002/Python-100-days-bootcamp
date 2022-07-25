1087a223fcaeb184da729ea4130988b2352e8b9c
Anupam Anand <aanand20@iitk.ac.in>
2022-07-25 16:39:48 +0530

    day 2

---
+++ Day 2/1.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+number = int(two_digit_number)
+sum = int(number/10) + (number%10)
+print(sum)
