f1381e92768bb2fcb9aed5d1af48377663b6088d
Anupam Anand <aanand20@iitk.ac.in>
2022-07-25 18:56:47 +0530

    day 3

---
+++ Day 3/4.py
@@ -0,0 +1,25 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if size == 'S':
+    price = int(15)
+    if add_pepperoni == 'Y':
+        price = price + 2
+elif size == 'M':
+    price = int(20)
+    if add_pepperoni == 'Y':
+        price = price + 3
+else:
+    price = int(25)
+    if add_pepperoni == 'Y':
+        price = price + 3
+
+if extra_cheese == 'Y':
+    price = price + 1
+
+print(f"Your final bill is: ${price}.")
